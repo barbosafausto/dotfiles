63a098bcd2049ea873c16fadab187326ccf95d37
faustus <54441045+barbosafausto@users.noreply.github.com>
2024-03-19 17:22:27 -0300

    Update launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,12 +3,14 @@
 # Terminate already running bar instances
 #killall -q polybar
 killall -q polybar
+while pgrep -u $UID -x polybar >/dev/nul; do sleep 1; done
 
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
 # Launch bar
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload main &
+done
 
 echo "Bars launched..."
